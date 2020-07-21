374bfe9f1e4a378761c8bfb970c633ebe2751824
leonardo bonifacio <leonardoqbonifacio@gmail.com>
2020-07-21 14:11:47 -0300

    programa em sua forma base inicial

---
+++ src/main/java/io/com/github/leodqb/MainClass.java
@@ -0,0 +1,12 @@
+package io.com.github.leodqb;
+//classe de inicialização da aplicação
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
